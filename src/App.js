7f623703be57e2bf951fec9f7446655a1e95971c
abhishekpanditt <badi.abhi@gmail.com>
2023-09-16 13:40:46 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Operations from "./Component/Operations";
+import "./style.css";
+
+let App = () => {
+    
+
+    return (
+        <div>
+            <Operations/>
+        </div>
+    );
+}
+export default App;
